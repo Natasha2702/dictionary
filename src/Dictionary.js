ac30befee55c33b8d17ea3e39a8f6728e591f81f
Natasha2702 <126653681+Natasha2702@users.noreply.github.com>
2025-08-29 15:15:34 +0200

    Added photos to the application

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,34 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import './Dictionary.css'; 
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults]= useState(null);
-  let [loaded, setLoaded]= useState(false)
+  let [loaded, setLoaded]= useState(false);
+  let [photos, setPhotos]=useState([]);
 
-  function handleResponse(response) { 
-    setResults(response.data);
+  
+  function handleImages(response){
+    setPhotos(response.data.photos)
   }
+
+function handleShecodesResponse(response) { 
+setResults(response.data);
+let shecodesApiKey="e6taed4cb9ef5dfoaff8f35a499504ef";
+let shecodesApiUrl=`https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesApiKey}`;
+axios
+.get(shecodesApiUrl, {headers: { Authorization: `Bearer ${shecodesApiKey}`}})
+.then(handleImages); }
+
 
 function search(){ let apiKey = "e6taed4cb9ef5dfoaff8f35a499504ef";
    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);}
+    axios.get(apiUrl).then(handleShecodesResponse);}
+  
+
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,7 +44,7 @@ function load(){
   search();
 }
 
-if (loaded){
+if (loaded) {
   return (
     <div className="Dictionary">
       <section>
@@ -48,6 +62,7 @@ if (loaded){
       </div>
       </section>
       <Results results={results}/>
+      <Photos photos={photos} />
     </div>
   );}else{
     load();
